3555b6cca14236e6bfa13f998f239737b097833d
Jonathan Bai <jonathanbai0317@outlook.com>
2020-02-26 13:55:39 -0500

    setting up componetns

--- reactapi/src/App.js
+++ reactapi/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form"
+import Weather from "./components/Weather"
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <Titles />
+        <Form />
+        <Weather />
+      </div>
+    );
+  }
 }
 
 export default App;
