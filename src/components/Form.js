e70b4db0c413fb12d5a7fee16e7ef4cac5f05f8a
Jonathan Bai <jonathanbai0317@outlook.com>
2020-03-02 12:10:40 -0500

    .

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,10 +5,10 @@ class Form extends React.Component{
     render(){
         return(
             <div className = "Form">
-                 <form onSubmit={this.props.getWeather}>
+                <form onSubmit={this.props.getWeather}>
                 <input type="text" name = "city" placeholder = "City" autoComplete = "on"/>
                 <input type="text" name = "country" placeholder = "Country" autoComplete = "on"/>
-                <button>Get Weather</button>
+                <button className="btn btn-primary">Get Weather</button>
             </form>
             </div>
            
