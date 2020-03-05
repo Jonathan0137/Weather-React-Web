b674da94dabb2dbdff316e926e1a254de3b3ebcb
Jonathan Bai <jonathanbai0317@outlook.com>
2020-03-05 11:23:46 -0500

    /,

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,22 +6,31 @@ class Weather extends React.Component{
         return(
             <div className = "container">
                 <div className = "content">
-                    {this.props.city && this.props.country && <h1>{this.props.city}, {this.props.country}</h1>}
-                    <h1 className = "icon">
-                        <i className = {`wi ${this.props.weatherIcon} display-1}`} />
+                    <h1 className = "country">
+                        {this.props.city && this.props.country && <p>{this.props.city}, {this.props.country}</p>}
                     </h1>
+
+                    <h1 className = "icon">
+                        <i className = {`wi ${this.props.weatherIcon} display-1`} />
+                    </h1>
+
                     <h1 className = "degree">
                         {this.props.temp && <p>{this.props.temp}°C</p>}
                     </h1>
+
                     <h4 className = "description">
                         {this.props.main && this.props.description && <p>{this.props.main}, {this.props.description}</p>}
                     </h4>
+
                     <h4 className = "humidity">
                         {this.props.humidity && <p>{this.props.humidity}% humidity</p>}
                     </h4>
-
+                    <h1 className = "error">
+                        {this.props.error && <p>{this.props.error}</p>}
+                    </h1>
+                    
                 </div>
-                {this.props.error && <h1>{this.props.error}</h1>}
+
             </div>
 
         )
